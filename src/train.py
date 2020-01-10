52ae9c1140b3e3e552f76fa4143d6f4958489b4d
kurianbenoy <kurian.bkk@gmail.com>
2020-01-10 13:22:21 +0530

    reorder files

---
+++ src/train.py
@@ -0,0 +1,59 @@
+import pandas as pd
+
+
+def encode_title(train, test, train_labels):
+    # encode title
+    train['title_event_code'] = list(map(lambda x, y: str(x) + '_' + str(y), train['title'], train['event_code']))
+    test['title_event_code'] = list(map(lambda x, y: str(x) + '_' + str(y), test['title'], test['event_code']))
+    all_title_event_code = list(set(train["title_event_code"].unique()).union(test["title_event_code"].unique()))
+    # make a list with all the unique 'titles' from the train and test set
+    list_of_user_activities = list(set(train['title'].unique()).union(set(test['title'].unique())))
+    # make a list with all the unique 'event_code' from the train and test set
+    list_of_event_code = list(set(train['event_code'].unique()).union(set(test['event_code'].unique())))
+    list_of_event_id = list(set(train['event_id'].unique()).union(set(test['event_id'].unique())))
+    # make a list with all the unique worlds from the train and test set
+    list_of_worlds = list(set(train['world'].unique()).union(set(test['world'].unique())))
+    # create a dictionary numerating the titles
+    activities_map = dict(zip(list_of_user_activities, np.arange(len(list_of_user_activities))))
+    activities_labels = dict(zip(np.arange(len(list_of_user_activities)), list_of_user_activities))
+    activities_world = dict(zip(list_of_worlds, np.arange(len(list_of_worlds))))
+    assess_titles = list(set(train[train['type'] == 'Assessment']['title'].value_counts().index).union(set(test[test['type'] == 'Assessment']['title'].value_counts().index)))
+    # replace the text titles with the number titles from the dict
+    train['title'] = train['title'].map(activities_map)
+    test['title'] = test['title'].map(activities_map)
+    train['world'] = train['world'].map(activities_world)
+    test['world'] = test['world'].map(activities_world)
+    train_labels['title'] = train_labels['title'].map(activities_map)
+    win_code = dict(zip(activities_map.values(), (4100*np.ones(len(activities_map))).astype('int')))
+    # then, it set one element, the 'Bird Measurer (Assessment)' as 4110, 10 more than the rest
+    win_code[activities_map['Bird Measurer (Assessment)']] = 4110
+    # convert text into datetime
+    train['timestamp'] = pd.to_datetime(train['timestamp'])
+    test['timestamp'] = pd.to_datetime(test['timestamp'])
+
+def read_data():
+    print('Reading train.csv file....')
+    train = pd.read_csv('input/train.csv')
+    print('Training.csv file have {} rows and {} columns'.format(train.shape[0], train.shape[1]))
+
+    print('Reading test.csv file....')
+    test = pd.read_csv('input/test.csv')
+    print('Test.csv file have {} rows and {} columns'.format(test.shape[0], test.shape[1]))
+
+    print('Reading train_labels.csv file....')
+    train_labels = pd.read_csv('input/train_labels.csv')
+    print('Train_labels.csv file have {} rows and {} columns'.format(train_labels.shape[0], train_labels.shape[1]))
+
+#    print('Reading specs.csv file....')
+#    specs = pd.read_csv('input/specs.csv')
+#    print('Specs.csv file have {} rows and {} columns'.format(specs.shape[0], specs.shape[1]))
+
+#    print('Reading sample_submission.csv file....')
+#    sample_submission = pd.read_csv('input/sample_submission.csv')
+#    print('Sample_submission.csv file have {} rows and {} columns'.format(sample_submission.shape[0], sample_submission.shape[1]))
+    return train, test, train_labels
+
+# read data
+train, test, train_labels, specs, sample_submission = read_data()
+# get usefull dict with maping encode
+train, test, train_labels, win_code, list_of_user_activities, list_of_event_code, activities_labels, assess_titles, list_of_event_id, all_title_event_code = encode_title(train, test, train_labels)
